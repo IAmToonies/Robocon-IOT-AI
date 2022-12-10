038274132a053558f9ea6ab6858a6b0fbe892ccf
Nguyễn Ngọc Nhân <88128495+IAmToonies@users.noreply.github.com>
2022-12-10 22:32:32 +0700

    Update Hw3.c

--- Code/Learn C/Hw3.c
+++ Code/Learn C/Hw3.c
@@ -1,3 +1,9 @@
+/*
+Bài 3:
+Nhập vào 1 số n, xong sau đó nhập vào 1 mảng gồm n phần tử.
+Viết hàm xử lí, đưa mảng vừa nhập vào hàm. Xử lí tìm giá trị lớn nhất có trong mảng và nhân tất cả các giá trị của mảng với giá trị lớn nhất đó
+*/
+
 # include<stdio.h>
 int promax(int *a, int *n, int *max){
 	for(int i = 0; i < *n ;i++){
