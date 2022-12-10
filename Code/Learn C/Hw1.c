62f099dc8826b2319b980cd2e92df10ba0ab9ddc
Nguyễn Ngọc Nhân <88128495+IAmToonies@users.noreply.github.com>
2022-12-10 22:30:21 +0700

    Update Hw1.c

--- Code/Learn C/Hw1.c
+++ Code/Learn C/Hw1.c
@@ -1,3 +1,7 @@
+/* Bài 1: 
+Nhập vào 2 số A, B. Viết một hàm con trỏ đối số đầu vào là địa chỉ của 2 số A, B, trong hàm xử lí cộng trực tiếp A vào B
+*/
+
 # include<stdio.h>
 int sum(int *a, int *b){
  	return *a + *b;
