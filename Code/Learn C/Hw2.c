89fe8785825e876bb3420af47f845b0e80dbce43
Nguyễn Ngọc Nhân <88128495+IAmToonies@users.noreply.github.com>
2022-12-10 22:31:44 +0700

    Update Hw2.c

--- Code/Learn C/Hw2.c
+++ Code/Learn C/Hw2.c
@@ -1,3 +1,23 @@
+/*
+Bài 2:
+Nhập vào 1 số n, xong sau đó nhập vào 1 mảng gồm n phần tử.
+Viết hàm xử lí, đưa mảng vừa nhập vào hàm. Xử lí và trả về giá trị lớn nhất trong mảng
+*/
+
+# include<stdio.h>
+int sum(int *a, int *b){
+ 	return *a + *b;
+ }
+void main(){
+	int a,b ;
+	printf("Input a and b: ");
+	scanf("%d %d",&a, &b);
+	printf("Total of a and b: %d ", sum(&a,&b));
+	
+}
+*/
+
+
 # include<stdio.h>
 int max(int *a, int *n){
 	int max=a[0];
